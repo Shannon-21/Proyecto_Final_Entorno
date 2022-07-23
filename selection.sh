92d4549510c325b8a64677e96e926543af38707b
Fabio Giampaoli <fabiogiampaoli21@gmail.com>
2022-07-23 13:43:18 -0300

    Aniado el script que ejecuta el menu de seleccion del archivo detexto y script de analisis

---
+++ selection.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# Defining the file text to analize
+read -p "Que archivo de texto desea analizar?: " FILE
+
+# check if the file exist and is not empty
+while [ ! -s $FILE ]; do
+	echo "el archivo $FILE no existe o es vacio"
+	read -p "Reintente por favor: " FILE
+done
+
+# Giving context to menu
+PS3='Elija: '
+echo "Con que script desea analizar el archivo $FILE?"
+
+select script in "Salir" "statsWords" "statsUsageWords" "findNames" "statsSentences" "blankLinesCounter"
+do
+	# It will run the scrip vinculated with number
+	[ $REPLY == 1 ] && echo "Hasta luego..." && sleep 1 && break
+	[ $REPLY == 2 ] && bash statsWords.sh $FILE
+	[ $REPLY == 3 ] && bash statsUsageWords.sh $FILE
+	[ $REPLY == 4 ] && bash findNames.sh $FILE
+	[ $REPLY == 5 ] && bash statsSentences.sh $FILE
+	[ $REPLY == 6 ] && bash blankLinesCounter.sh $FILE
+	[ $REPLY -ge 7 ] && echo "Seleccion incorrecta" && continue  
+done
+
+exit 0
